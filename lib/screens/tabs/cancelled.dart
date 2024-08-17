2497edbd249ad5dd58d806e42ed90b730f03a206
fwdAndroid <fwdkaleem@gmail.com>
2024-08-17 15:46:57 +0500

    Appointments

--- lib/screens/tabs/cancelled.dart
+++ lib/screens/tabs/cancelled.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:medicare_doctor/uitls/colors.dart';
@@ -13,106 +15,149 @@ class _CancelledState extends State<Cancelled> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: (context, index) {
-        return SizedBox(
-          height: 120,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      "assets/logo.png",
-                      height: 90,
-                      width: 90,
-                    ),
-                    const SizedBox(
-                      width: 7,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Mohammad Quasi",
-                              style: GoogleFonts.poppins(
-                                  fontSize: 16,
-                                  color: appColor,
-                                  fontWeight: FontWeight.w600),
-                            ),
-                            const SizedBox(
-                              width: 3,
-                            ),
-                            Container(
-                              width: 80,
-                              height: 24,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: cancelColor),
-                                  borderRadius: BorderRadius.circular(4)),
-                              child: Center(
-                                child: Text(
-                                  "Cancelled",
-                                  style: GoogleFonts.poppins(
-                                      fontSize: 12, color: cancelColor),
-                                ),
-                              ),
-                            )
-                          ],
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("appointments")
+              .where("doctorid",
+                  isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+              .where("appointmentStatus", isEqualTo: "cancel")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Text(
+                  "No Appointment Available",
+                  style: TextStyle(color: black),
+                ),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  final Map<String, dynamic> data =
+                      documents[index].data() as Map<String, dynamic>;
+
+                  return Column(
+                    children: [
+                      SizedBox(
+                        height: 120,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.network(
+                                    data['doctorPhoto'],
+                                    height: 90,
+                                    width: 90,
+                                  ),
+                                  const SizedBox(
+                                    width: 7,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(
+                                            data['paitientName'],
+                                            style: GoogleFonts.poppins(
+                                                fontSize: 16,
+                                                color: appColor,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          const SizedBox(
+                                            width: 3,
+                                          ),
+                                          Container(
+                                            width: 80,
+                                            height: 24,
+                                            decoration: BoxDecoration(
+                                                border: Border.all(
+                                                    color: cancelColor),
+                                                borderRadius:
+                                                    BorderRadius.circular(4)),
+                                            child: Center(
+                                              child: Text(
+                                                "Cancelled",
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 12,
+                                                    color: mainColor),
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      Text(
+                                        "+82312412414424",
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 14,
+                                            color: appColor,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                      const SizedBox(
+                                        height: 30,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          Text(
+                                            data['appointmentDate'],
+                                            style: GoogleFonts.poppins(
+                                                color: dateColor,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w300),
+                                          ),
+                                          const SizedBox(
+                                            width: 6,
+                                          ),
+                                          Text(
+                                            "|",
+                                            style: GoogleFonts.poppins(
+                                                color: dateColor,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w300),
+                                          ),
+                                          const SizedBox(
+                                            width: 6,
+                                          ),
+                                          Text(
+                                            data['appointmentTime'],
+                                            style: GoogleFonts.poppins(
+                                                color: dateColor,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w300),
+                                          )
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                        Text(
-                          "+82312412414424",
-                          style: GoogleFonts.poppins(
-                              fontSize: 14,
-                              color: appColor,
-                              fontWeight: FontWeight.w400),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Divider(
+                          color: dividerColor,
                         ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              "23 Sept 2023",
-                              style: GoogleFonts.poppins(
-                                  color: dateColor,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300),
-                            ),
-                            const SizedBox(
-                              width: 6,
-                            ),
-                            Text(
-                              "|",
-                              style: GoogleFonts.poppins(
-                                  color: dateColor,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300),
-                            ),
-                            const SizedBox(
-                              width: 6,
-                            ),
-                            Text(
-                              "12:30 AM",
-                              style: GoogleFonts.poppins(
-                                  color: dateColor,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w300),
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        );
-      }),
+                      )
+                    ],
+                  );
+                });
+          }),
     );
   }
 }
--- lib/screens/tabs/completed.dart
+++ lib/screens/tabs/completed.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:medicare_doctor/uitls/colors.dart';
@@ -13,127 +15,158 @@ class _CompletedState extends State<Completed> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: (context, index) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 120,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Row(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("appointments")
+              .where("doctorid",
+                  isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+              .where("appointmentStatus", isEqualTo: "complete")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Text(
+                  "No Appointment Available",
+                  style: TextStyle(color: black),
+                ),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  final Map<String, dynamic> data =
+                      documents[index].data() as Map<String, dynamic>;
+                  return Card(
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Image.asset(
-                          "assets/logo.png",
-                          height: 90,
-                          width: 90,
-                        ),
-                        const SizedBox(
-                          width: 7,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
+                        SizedBox(
+                          height: 170,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
                               children: [
-                                Text(
-                                  "Mohammad Quasi",
-                                  style: GoogleFonts.poppins(
-                                      fontSize: 16,
-                                      color: appColor,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                const SizedBox(
-                                  width: 3,
-                                ),
-                                Container(
-                                  width: 80,
-                                  height: 24,
-                                  decoration: BoxDecoration(
-                                      border: Border.all(color: completeColor),
-                                      borderRadius: BorderRadius.circular(4)),
-                                  child: Center(
-                                    child: Text(
-                                      "Completed",
-                                      style: GoogleFonts.poppins(
-                                          fontSize: 12, color: completeColor),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Image.network(
+                                      data['doctorPhoto'],
+                                      height: 90,
+                                      width: 90,
                                     ),
-                                  ),
+                                    const SizedBox(
+                                      width: 7,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(
+                                              data['paitientName'],
+                                              style: GoogleFonts.poppins(
+                                                  fontSize: 16,
+                                                  color: appColor,
+                                                  fontWeight: FontWeight.w600),
+                                            ),
+                                            const SizedBox(
+                                              width: 3,
+                                            ),
+                                            Container(
+                                              width: 80,
+                                              height: 24,
+                                              decoration: BoxDecoration(
+                                                  border: Border.all(
+                                                      color: completeColor),
+                                                  borderRadius:
+                                                      BorderRadius.circular(4)),
+                                              child: Center(
+                                                child: Text(
+                                                  "Completed",
+                                                  style: GoogleFonts.poppins(
+                                                      fontSize: 12,
+                                                      color: completeColor),
+                                                ),
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                        Text(
+                                          "+82312412414424",
+                                          style: GoogleFonts.poppins(
+                                              fontSize: 14,
+                                              color: appColor,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                        const SizedBox(
+                                          height: 30,
+                                        ),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            Text(
+                                              data['appointmentDate'],
+                                              style: GoogleFonts.poppins(
+                                                  color: dateColor,
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w300),
+                                            ),
+                                            const SizedBox(
+                                              width: 6,
+                                            ),
+                                            Text(
+                                              "|",
+                                              style: GoogleFonts.poppins(
+                                                  color: dateColor,
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w300),
+                                            ),
+                                            const SizedBox(
+                                              width: 6,
+                                            ),
+                                            Text(
+                                              data['appointmentTime'],
+                                              style: GoogleFonts.poppins(
+                                                  color: dateColor,
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w300),
+                                            )
+                                          ],
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 8.0, bottom: 4),
+                                          child: OutlinedButton(
+                                              style: OutlinedButton.styleFrom(
+                                                  foregroundColor: mainColor),
+                                              onPressed: () {},
+                                              child: Text(
+                                                "Review",
+                                                style: GoogleFonts.poppins(
+                                                    color: mainColor),
+                                              )),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 )
                               ],
                             ),
-                            Text(
-                              "+82312412414424",
-                              style: GoogleFonts.poppins(
-                                  fontSize: 14,
-                                  color: appColor,
-                                  fontWeight: FontWeight.w400),
-                            ),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "23 Sept 2023",
-                                  style: GoogleFonts.poppins(
-                                      color: dateColor,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                                const SizedBox(
-                                  width: 6,
-                                ),
-                                Text(
-                                  "|",
-                                  style: GoogleFonts.poppins(
-                                      color: dateColor,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300),
-                                ),
-                                const SizedBox(
-                                  width: 6,
-                                ),
-                                Text(
-                                  "12:30 AM",
-                                  style: GoogleFonts.poppins(
-                                      color: dateColor,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300),
-                                )
-                              ],
-                            ),
-                          ],
+                          ),
                         ),
                       ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Divider(
-                color: dividerColor,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, bottom: 4),
-              child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(foregroundColor: mainColor),
-                  onPressed: () {},
-                  child: Text(
-                    "Review",
-                    style: GoogleFonts.poppins(color: mainColor),
-                  )),
-            )
-          ],
-        );
-      }),
+                    ),
+                  );
+                });
+          }),
     );
   }
 }
